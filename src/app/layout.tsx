bc78c5d6bb0389897828c73ee5628d969277c017
Vrashank Shetty <vrashankshetty.s@gmail.com>
2023-11-07 19:24:43 +0530

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,14 @@ import { CartProvider } from "@/context";
 import Provider from "@/components/provider";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { useSession } from "next-auth/react";
 
 // import { AppProvider } from "@/context";
 // import { cartState } from "@/@types/globalTypes";
 // import { useEffect, useReducer, useState } from "react";
 // import { initialState, reducerFn } from "@/reducer";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
